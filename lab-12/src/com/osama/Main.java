16660743c606c473e8d5bf4343fa9e4afc7c1558
Os-ama <k201725@nu.edu.pk>
2022-12-05 08:43:33 +0500

    Hello Data

---
+++ lab-12/src/com/osama/Main.java
@@ -0,0 +1,12 @@
+package com.osama;
+
+public class Main {
+	public int a=10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main object = new Main();
+		System.out.println("Hello Data, "+object.a);
+	}
+
+}
